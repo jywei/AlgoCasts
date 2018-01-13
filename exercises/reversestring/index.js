fc1087687f2ec29c4fc164516c84379a20b46a45
Roy Wei <roywjy@gmail.com>
2018-01-13 22:52:44 +0800

    Refactor and try debugger

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -7,11 +7,11 @@
 //   reverse('Greetings!') === '!sgniteerG'
 
 function reverse(str) {
-  return str.split('').reduce((rev, char) => 
-    char + rev, '')
+  return str.split('').reduce((rev, char) => char + rev, '')
 }
 
 module.exports = reverse;
+// reverse('asdf')
 
 // function reverse(str) {
 //   return str.split('').reverse().join('')
