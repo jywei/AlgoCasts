6537540dd6072429003090d3e9e51cf5bdbb1250
Roy Wei <roywjy@gmail.com>
2018-01-21 16:02:21 +0800

    Add second solution for palindrome

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,9 +7,15 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
+// function palindrome(str) {
+//   const reversed = str.split('').reverse().join('')
+//   return str === reversed
+// }
+
 function palindrome(str) {
-  const reversed = str.split('').reverse().join('')
-  return str === reversed
+  return str.split('').every((char, index) => {
+    return char === str[str.length - index - 1]
+  })
 }
 
 module.exports = palindrome;
