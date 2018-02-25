9c5b8aaf60588e96c82a587286e1b9f00251a75e
Roy Wei <roywjy@gmail.com>
2018-02-25 16:01:11 +0800

    Add steps solution two

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,18 +17,28 @@
 //       '### '
 //       '####'
 
-function steps(n) {
-  for (let row = 0; row < n; row++) {
-    let stair = ''
+// function steps(n) {
+//   for (let row = 0; row < n; row++) {
+//     let stair = ''
+//
+//     for (let column = 0; column < n; column++) {
+//       if (column <= row) {
+//         stair += '#'
+//       } else {
+//         stair += ' '
+//       }
+//     }
+//     console.log(stair)
+//   }
+// }
 
-    for (let column = 0; column < n; column++) {
-      if (column <= row) {
-        stair += '#'
-      } else {
-        stair += ' '
-      }
-    }
+function steps(n) {
+  let i = 1
+  while (i <= n) {
+    let stair = ' '
+    stair = '#'.repeat(i) + ' '.repeat(n - i)
     console.log(stair)
+    i++
   }
 }
 
