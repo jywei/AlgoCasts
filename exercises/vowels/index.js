e08b4b7b285f8861b03a4928ffcab2826623765a
Roy Wei <roywjy@gmail.com>
2018-02-25 18:07:17 +0800

    Add vowels solution two regular expression

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,17 +7,23 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
+// function vowels(str) {
+//   let count = 0
+//   const checker = 'aeiou'
+//
+//   for (let char of str.toLowerCase()) {
+//     if (checker.includes(char)) {
+//       count++
+//     }
+//   }
+//
+//   return count
+// }
+
 function vowels(str) {
-  let count = 0
-  const checker = 'aeiou'
-
-  for (let char of str.toLowerCase()) {
-    if (checker.includes(char)) {
-      count++
-    }
-  }
-
-  return count
+  const matches = str.match(/[aeiou]/gi)
+  return matches ? matches.length : 0
 }
+
 
 module.exports = vowels;
