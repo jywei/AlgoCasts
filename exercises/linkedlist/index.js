84ba4dfddd46a6903baf26d1661e1f9420e2fb8e
Roy Wei <roywjy@gmail.com>
2019-05-12 22:35:30 +1000

    Fix insertLast and add getAt

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -82,10 +82,28 @@ class LinkedList {
   }
 
   insertLast(data) {
-    if (this.head == null) {
-      this.insertFirst(data);
+    const last = this.getLast();
+
+    if (last) {
+      // There are some existing nodes in our chain
+      last.next = new Node(data);
+    } else {
+      // The chain is empty!
+      this.head = new Node(data);
     }
-    this.getLast().next = new Node(data, null);
+  }
+
+  getAt(index) {
+    let counter = 0;
+    let node = this.head;
+    while (node) {
+      if (counter === index) {
+        return node;
+      }
+      counter++;
+      node = node.next;
+    }
+    return null;
   }
 }
 
